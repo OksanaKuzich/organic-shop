df340106f46630bfc93d03a65a709533545817da
OksanaKuzich <komoks255@gmail.com>
2023-05-04 21:13:05 +0200

    add func for button

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,14 +1,26 @@
 import { Button } from 'components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 import { Container } from 'components/Container/Container.styled';
 import { HeroSection, Subtitle, Title } from './Hero.styled';
 
 export const Hero = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/*', { replace: true });
+  };
+
   return (
     <HeroSection>
       <Container>
         <Subtitle>100% Natural Food</Subtitle>
         <Title>Choose the best healthier way of life</Title>
-        <Button text={'Explore Now'} type="button" stl={'blue'}></Button>
+        <Button
+          text={'Explore Now'}
+          type="button"
+          stl={'blue'}
+          func={handleButtonClick}
+        ></Button>
       </Container>
     </HeroSection>
   );
